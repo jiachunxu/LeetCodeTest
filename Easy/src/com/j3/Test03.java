0fe12a60d1773879880e98adcb823b61896d156c
jiacx <jiacx@qq.com>
2021-10-01 16:19:50 +0800

    简单第3题 commit

---
+++ Easy/src/com/j3/Test03.java
@@ -0,0 +1,50 @@
+package com.j3;
+
+/**
+ * @author: JCX
+ * @DATE: 2021/10/1 - 10 - 01 - 15:51
+ * 9. 回文数
+ *
+ */
+public class Test03 {
+    public static void main(String[] args) {
+
+        Solution ss = new Solution();
+        System.out.println(ss.isPalindrome(12321));
+
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        /*
+        利用了7.整数反转的偷懒办法
+        */
+        if (x < 0) {
+            return false;
+        } else {
+            int dd = x;
+
+
+            long res = 0;
+            int temp;
+
+            while (0 != x / 10) {
+
+                temp = x % 10;
+                res = res * 10 + temp;
+                x = x / 10;
+
+            }
+            res = res * 10 + x;
+            if (dd==res){
+                return true;
+            }else {
+                return false;
+            }
+
+
+        }
+
+    }
+}
