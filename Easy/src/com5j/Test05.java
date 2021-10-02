2e6cd9989a43092730e10cfa096fbf6d4f5f71ab
jiacx <jiacx@qq.com>
2021-10-02 19:44:30 +0800

    0 commit

---
+++ Easy/src/com5j/Test05.java
@@ -0,0 +1,12 @@
+package com5j;
+
+/**
+ * @author JCX
+ * 时间: 2021/10/2 - 10 - 02 - 19:44
+ * 包名: com5j
+ */
+public class Test05 {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
