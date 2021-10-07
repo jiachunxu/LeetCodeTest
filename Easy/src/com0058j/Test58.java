eb7bab8eabd7ea1607d8ca59f221610a5ef2c53c
jiacx <jiacx@qq.com>
2021-10-07 12:56:21 +0800

    简单58

---
+++ Easy/src/com0058j/Test58.java
@@ -0,0 +1,36 @@
+package com0058j;
+
+/**
+ * @author JCX
+ * 时间: 2021/10/7 - 10 - 07 - 12:48
+ * 58. 最后一个单词的长度
+ * https://leetcode-cn.com/problems/length-of-last-word/submissions/
+ */
+public class Test58 {
+    public static void main(String[] args) {
+        Solution ss = new Solution();
+        String s = "";
+        System.out.println(ss.lengthOfLastWord(s));
+    }
+}
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int len = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            if (s.charAt(i) != ' ') {
+
+                len++;
+
+
+            } else if (len != 0) {
+
+                return len;
+            }
+
+
+        }
+        return len;
+    }
+}
