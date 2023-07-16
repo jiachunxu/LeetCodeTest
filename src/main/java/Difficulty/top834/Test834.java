3d8e528ab83c8c88876eda90e06afd864b135be2
jiacx <954951185@qq.com>
2023-07-16 20:05:00 +0800

    0

---
+++ src/main/java/Difficulty/top834/Test834.java
@@ -0,0 +1,22 @@
+package Difficulty.top834;
+
+/**
+ * @author adminjia
+ * <p>
+ * 834. 树中距离之和
+ * <p>
+ * https://leetcode.cn/problems/sum-of-distances-in-tree/
+ */
+public class Test834 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+
+
+        return new int[0];
+    }
+}
